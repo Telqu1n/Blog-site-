eccd65b96540dd5af33e8829cf91e69c6ccfac83
Jack Carney <jackcarney09@outlook.com>
2024-11-25 11:34:37 +0000

    Adding the names to the urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .import views 
 
-urlspattern =[
-  path(''),
-  path('posts'),
-  path ('posts/slug:slug>') # <slug> is a placeholder for the actual slug value/ e.g. posts/my-first-post. 
+
+urlpatterns = [
+  path('', views.starting_page,  name='starting-page'),
+  path('posts', views.posts, name='posts-page'),
+  path('posts/<slug:slug>', views.post_detail, name='post-detail-page' )
+  # <slug> is a placeholder for the actual slug value/ e.g. posts/my-first-post. 
 ]
 
