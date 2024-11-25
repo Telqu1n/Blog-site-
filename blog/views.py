fd09e6a3ccbcf37452d7f21c57661977af725bd4
Jack Carney <jackcarney09@outlook.com>
2024-11-25 14:25:34 +0000

    Adding a variable with a dictionary in to save data.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,18 @@
+from datetime import date
 from django.shortcuts import render
+
+posts = [
+  {
+    "slug": "Hike-in-the-mountains",
+    "image": "mountains.jpg",
+    "author": "Jack",
+    "date": date(2024, 11, 25),
+    "title": "Mountain Hiking",
+    "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened...",
+    "content": "This is the content of the blog post. I'll write more later."
+  }
+]
+
 
 # Create your views here.
 def starting_page (request):
