3938af90a5c1b1b5f4f979488c5f2960ac7c687f
Jack Carney <jackcarney09@outlook.com>
2024-12-04 14:02:15 +0000

    adding a filter in the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -4,11 +4,13 @@ from .models import Post, Tag, Author
 
 class PostAdmin (admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "date",)
     list_display = ("title", "author", "date",)
 
 
 class AuthorAdmin (admin.ModelAdmin):
     list_display = ("full_name", "email",)
+    list_filter = ("last_name","email",)
     
 
 admin.site.register(Post, PostAdmin)
